98a9c44a677d3fd92c30b7e5e0b1a66d4ab5425f
Elen <elen.ghazaryan6@gmail.com>
2022-04-03 01:12:11 +0400

    feat

--- src/App.js
+++ src/App.js
@@ -1,37 +1,24 @@
-import React, { useReducer, useContext } from "react";
+import React from "react";
 import { Route, Routes } from "react-router-dom";
+import { ContextProvider } from "./state";
+
 import "./App.css";
 import SignIn from "./pages/SignIn";
 import Welcome from "./pages/Welcome";
 import Home from "./pages/Home";
 import SingleBoard from "./pages/SingleBoard";
-import { ContextProvider, useCustomContext } from "./state";
-import Modal from "./components/Modal/Modal";
-
-// export const DispatchContext = React.createContext();
-// export const BoardsContext = React.createContext();
-// export const StateContext = React.createContext();
 
 function App() {
-  // const [state, dispatch] = useReducer(reducer, defaultState);
-
   return (
     <div className="App">
       <ContextProvider>
-        {/* <StateContext.Provider value={state}>
-        <BoardsContext.Provider value={state.boards}>
-          <DispatchContext.Provider value={dispatch}> */}
         <Routes>
           <Route index element={<Welcome />} />
           <Route path="sign-in" element={<SignIn />} />
           <Route path="/home" element={<Home />} />
           <Route path="board-item" element={<SingleBoard />} />
         </Routes>
-        {/* {state.isModalOpen && <Modal {...state.modalInfo} />} */}
       </ContextProvider>
-      {/* </DispatchContext.Provider>
-        </BoardsContext.Provider>
-      </StateContext.Provider> */}
     </div>
   );
 }
